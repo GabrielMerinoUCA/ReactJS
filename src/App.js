e894277bdd7c8f245fd494deb2853ee618fc088f
GabrielMerinoUCA <gabriel.merino13079@est.uca.edu.ni>
2024-12-13 05:49:02 -0330

    Third React tutorial finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to my blog";
+  let likes = 50;
+  const link = "https://github.com/GabrielMerinoUCA/ReactJS";
   return (
     <div className="App">
       <div className="content">
-        <h1>App component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{"Hello there"}</p>
+        <a href={link}>repo</a>
       </div>
     </div>
   );
