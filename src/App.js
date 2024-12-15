d78afd10c3483ef03721ea6c83aa58f2c7d9d1a3
GabrielMerinoUCA <gabriel.merino13079@est.uca.edu.ni>
2024-12-14 23:12:52 -0330

    Fifth React tutorial finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import './App.css';
 import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  let likes = 50;
-  const link = "https://github.com/GabrielMerinoUCA/ReactJS";
   return (
     <div className="App">
       <Navbar/>
       <div className="content">
         <Home/>
-        <p>Liked {likes} times</p>
-        <p>{"Hello there"}</p>
-        <a href={link}>repo</a>
       </div>
     </div>
   );
