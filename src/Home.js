83caecbdade80f1601167cff77425006d709d750
GabrielMerinoUCA <gabriel.merino13079@est.uca.edu.ni>
2024-12-15 03:52:08 -0330

    Sixth React tutorial finished

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const clickHandler = (e) => {
+        console.log("Hello world", e);
+    }
+
+    const secondClickHandler = (name, e) => {
+        console.log("Hello " + name, e)
+    }
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={clickHandler}>Click Me!</button>
+            <button onClick={(e) => secondClickHandler("name", e)}>Click Me again!</button>
         </div>
      );
 }
