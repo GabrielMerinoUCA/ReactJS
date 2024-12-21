56303c2ddf3dc6aa2a359fbc5c112c14793710e8
GabrielMerinoUCA <gabriel.merino13079@est.uca.edu.ni>
2024-12-20 21:01:14 -0330

    Ninth React tutorial finished

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
-import {useState} from "react"
+import {useState} from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -7,18 +8,9 @@ const Home = () => {
         {title: "Web dev tool tips", author: "Anon" , body: "lorem ipsum...", id: 3},
     ]);
 
-    
-
     return ( 
         <div className="home">
-            {blogs.map((blog) => {
-                return (
-                    <div className="blog-preview" key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <p>Written by: {blog.author}</p>
-                    </div>
-                );
-            })}
+            <BlogList blogs={blogs} title="All blogs" />
         </div>
      );
 }
