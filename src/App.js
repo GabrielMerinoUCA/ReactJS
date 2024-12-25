73d4a51258029d0539d77b5c0232f5df4758dfa1
GabrielMerinoUCA <gabriel.merino13079@est.uca.edu.ni>
2024-12-25 18:59:29 -0330

    Sixteenth React tutorial finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import Create from "./Create";
 import {BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -9,15 +10,18 @@ function App() {
         <Navbar />
         <div className="content">
           {/* 
-          Routes is a component that allows routing from a list of other components. It prevents more
+          Switch is a component that allows routing from a list of other components. It prevents more
           than one route from displaying at the same time. The way it works is that depending on the
           user, it will select one of the target routes to replace itself on the final dom so it only
           uses the specified area. It's like having responsible sections instead of changing the whole page
           */}
           <Switch>
-            <Route path="/" >
+            <Route exact path="/" >
               <Home />
-            </Route>              
+            </Route>
+            <Route exact path="/create">
+              <Create />
+            </Route>  
           </Switch>
         </div>
       </div>
