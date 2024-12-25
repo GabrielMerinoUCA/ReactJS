7a41be5eeddc4428cd4b05ff6ba1341e2a291f35
GabrielMerinoUCA <gabriel.merino13079@est.uca.edu.ni>
2024-12-25 18:36:19 -0330

    Fifteenth React tutorial finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import {BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home/>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/* 
+          Routes is a component that allows routing from a list of other components. It prevents more
+          than one route from displaying at the same time. The way it works is that depending on the
+          user, it will select one of the target routes to replace itself on the final dom so it only
+          uses the specified area. It's like having responsible sections instead of changing the whole page
+          */}
+          <Switch>
+            <Route path="/" >
+              <Home />
+            </Route>              
+          </Switch>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,13 @@ import useFetch from "./useFetch";
 
 
 const Home = () => {
-    const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs"); // using objs allows us to only get some of the variables instead of all of them
+    const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs"); // using objs allows us to only get some of the variables instead of all of them    
     return (
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All blogs"/>}
         </div>
-        
      );
 }
  
