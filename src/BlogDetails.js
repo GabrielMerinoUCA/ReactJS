4ec550e170042b894d1bc12f09c9fc093e50f315
GabrielMerinoUCA <gabriel.merino13079@est.uca.edu.ni>
2024-12-26 02:07:44 -0330

    Twentieth React tutorial finished

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -4,10 +4,24 @@ import useFetch from "./useFetch";
 const BlogDetails = () => {
     // this lets use grab parameters from the route
     const {id} = useParams();
-    
+    /* apparently, json server is capable of retrieving single objects without further complications.
+    You just have to put the id and it automatically know to look for an id attribute in the json
+    file */
+    const {data: blog, isPending, error} = useFetch("http://localhost:8000/blogs/" + id);
 
-    return ( 
-        <h2>Blog Details - {id}</h2>
+    return (
+        <div className="blog-details">  
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
+        </div>
+
     );
 }
  
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,8 +4,8 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>The Blogs</h1>
             <div className="links">
-                <Link to="./">Home</Link>
-                <Link to="./create">New Blog</Link>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
      );
